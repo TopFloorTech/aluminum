78f7732f3db3eaba2800d8eefb6ad2dec9f514e5
Ben McClure <ben.mcclure@gmail.com>
2016-10-08 21:38:26 -0500

    Add InvokableTrait as a base to save some repeated typing.

---
+++ src/Aluminum/Traits/InvokableTrait.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BMcClure
+ * Date: 10/8/2016
+ * Time: 9:35 PM
+ */
+
+namespace Drupal\aluminum\Aluminum\Traits;
+
+
+trait InvokableTrait {
+  protected function invokeHook($hookName) {
+    $moduleHandler = \Drupal::moduleHandler();
+
+    $data = $moduleHandler->invokeAll($hookName);
+    $moduleHandler->alter($hookName, $data);
+
+    return $data;
+  }
+}
